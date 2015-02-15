e6f8d1a44007dc755f02316525614efc85ab5c26
Michael Mrozek <git@mrozekma.com>
2015-02-15 02:33:15 -0500

    Daemonize by default

--- Config.py
+++ Config.py
@@ -6,6 +6,8 @@ FILENAME = expanduser('~/.helm')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--conf', help = 'Configuration file', dest = 'conf', default = FILENAME)
+parser.add_argument('-n', '--no-detach', help = 'Stay in foreground', action = 'store_false', dest = 'daemon', default = True)
+parser.add_argument('--pidfile', help = 'Write PID to this file', dest = 'pidfile', default = None)
 parser.add_argument('--test', action = 'store_true', dest = 'test', default = False)
 args = parser.parse_args()
 
@@ -21,3 +23,6 @@ def get(section, option, default = NO_DEFAULT):
 			raise
 		else:
 			return default
+
+def has(section, option):
+	return config.has_option(section, option)
--- helm.py
+++ helm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+import os
+
 from Backend import Backend
-from Config import get as getConfig
+from Config import get as getConfig, has as hasConfig
 
 backend = Backend(
 	getConfig('server', 'server', 'helm.mrozekma.com'),
@@ -12,9 +14,42 @@ backend = Backend(
 from Config import args
 if args.test:
 	backend.send({'foo': 'bar'})
+	exit(0)
 else:
 	def cb(data):
 		print "receive: %s" % data
 	backend.onReceive(cb)
+
+if args.daemon:
+	if hasConfig('host', 'log'):
+		logFile = getConfig('host', 'log')
+	elif os.access('/var/log/helm', os.W_OK):
+		logFile = '/var/log/helm/helm.log'
+	elif os.access('/tmp', os.W_OK):
+		logFile = '/tmp/helm.log'
+	else:
+		logFile = None
+
+	if logFile is not None:
+		print "Logging to %s" % logFile
+
+	# Double-fork
+	if os.fork() != 0:
+		os._exit(0)
+	os.setsid()
+	if os.fork() != 0:
+		os._exit(0)
+
+	# Point the standard file descriptors at a log file
+	if logFile is not None:
+		log = os.open(logFile, os.O_CREAT | os.O_RDWR)
+		os.dup2(log, 0)
+		os.dup2(log, 1)
+		os.dup2(log, 2)
+
+pidfile = args.pidfile or getConfig('host', 'pidfile', None)
+if pidfile is not None:
+	with open(pidFile, 'w') as f:
+		f.write("%d\n" % os.getpid())
 
 while True: pass
