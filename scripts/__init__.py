0c3bbbb9fc2074e400e181b87bda2d8c2b0e3112
Michael Mrozek <git@mrozekma.com>
2015-03-21 20:12:52 -0400

    Fix relative pathing of scripts directory

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
 from os import listdir
+from os.path import dirname
 
-__all__ = map(lambda x: x[:-3], filter(lambda x: x[-3:] == '.py' and x != '__init__.pyc' and x[0] != '#' and x[0] != '.', listdir('scripts')))
-
+__all__ = map(lambda x: x[:-3], filter(lambda x: x[-3:] == '.py' and x != '__init__.pyc' and x[0] != '#' and x[0] != '.', dirname(__file__)))
