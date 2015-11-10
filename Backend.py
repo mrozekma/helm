9a54090fc224e7d05482d28bcf463325ff3a6872
Michael Mrozek <git@mrozekma.com>
2015-11-10 01:11:55 -0500

    Reconnect when RabbitMQ connection is lost

--- Backend.py
+++ Backend.py
@@ -4,26 +4,32 @@ import pika
 from threading import Thread
 
 class ReceiveThread(Thread):
-	def __init__(self, chan, queue, cb):
+	def __init__(self, backend, chan, queue, cb):
 		Thread.__init__(self)
+		self.backend = backend
 		self.chan = chan
 		self.queue = queue
 		self.cb = cb
 		self.daemon = True
 
 	def run(self):
-		for method, properties, body in self.chan.consume(self.queue):
-			result = self.cb(fromJS(body))
-			if result != False:
-				self.chan.basic_ack(method.delivery_tag)
+		try:
+			for method, properties, body in self.chan.consume(self.queue):
+				result = self.cb(fromJS(body))
+				if result != False:
+					self.chan.basic_ack(method.delivery_tag)
+		except pika.exceptions.ConnectionClosed:
+			# Reconnect and start a new thread
+			print "Connection closed; reconnecting"
+			self.backend.connect()
+			self.backend.onReceive(self.cb)
 
 class Backend:
 	def __init__(self, host, exchange, username, password):
 		self.exchange = exchange
-		creds = pika.PlainCredentials(username, password)
-		self.conn = pika.BlockingConnection(pika.ConnectionParameters(host = host, credentials = creds))
-		self.chan = self.conn.channel()
-		self.chan.exchange_declare(exchange = self.exchange, type = 'fanout')
+		self.host = host
+		self.creds = pika.PlainCredentials(username, password)
+		self.connect()
 
 	def __del__(self):
 		try:
@@ -34,10 +40,15 @@ class Backend:
 		except TypeError:
 			pass
 
+	def connect(self):
+		self.conn = pika.BlockingConnection(pika.ConnectionParameters(host = self.host, credentials = self.creds))
+		self.chan = self.conn.channel()
+		self.chan.exchange_declare(exchange = self.exchange, type = 'fanout')
+
 	def send(self, data):
 		self.chan.basic_publish(exchange = self.exchange, routing_key = '', body = toJS(data))
 
 	def onReceive(self, cb):
 		queue = self.chan.queue_declare().method.queue
 		self.chan.queue_bind(exchange = self.exchange, queue = queue)
-		ReceiveThread(self.chan, queue, cb).start()
+		ReceiveThread(self, self.chan, queue, cb).start()
